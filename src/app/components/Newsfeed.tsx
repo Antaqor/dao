390fdc3c97e635a1ebe8abac85a63f973038f703
Antaqor <antaqor@gmail.com>
2024-12-09 15:52:42 +0800

    Post function хийж нэмж хөгжүүлэв

--- src/app/components/Newsfeed.tsx
+++ src/app/components/Newsfeed.tsx
@@ -99,7 +99,7 @@ const Newsfeed: React.FC = () => {
                         onClick={createPost}
                         className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
                     >
-                        Post
+                        Postt
                     </button>
                 </div>
 
