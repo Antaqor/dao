ecaf777154c31dc6265609f8b97d0e14e8d04b43
Antaqor <antaqor@gmail.com>
2025-01-08 21:17:25 +0800

    payment integrated

--- src/app/salons/[id]/page.tsx
+++ src/app/salons/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useParams } from "next/navigation";
 import {
